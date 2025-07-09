9d9058392b801493fde83af4b7220382d5edfdf5
ErGovindthakur <ergovindthakur@gmail.com>
2025-07-09 22:58:35 +0530

    setup db and connect it with server

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 import express from "express"
-
+import connectDB from "./config/db.js";
+import dotenv from "dotenv"
+dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 5050;
@@ -7,6 +9,8 @@ const port = process.env.PORT || 5050;
 app.get('/',(req,res)=>{
      res.send("Working")
 })
+
+connectDB();
 app.listen(port,()=>{
      console.log(`server is running at http://localhost:${port}`)
 })
